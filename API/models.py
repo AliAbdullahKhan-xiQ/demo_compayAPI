de14e9aa6ff598378697be0788812d8d603412b6
AliAbdullahKhan-xiQ <131864775+AliAbdullahKhan-xiQ@users.noreply.github.com>
2023-06-05 10:26:02 +0500

    employee was added

--- API/models.py
+++ API/models.py
@@ -16,3 +16,21 @@ class Company(models.Model):
     ))
     add_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+# employee model
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    about = models.TextField()
+    position = models.CharField(max_length=50, choices=(
+        ('Manager', 'manage'),
+        ('Software Developer', 'sd'),
+        ('Project Leader', 'pl')
+    ))
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
